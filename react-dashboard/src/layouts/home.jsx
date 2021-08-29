ef556df65364cef30b60026461bf2b1717026bf3
Avery Hampton <averydhampton@icloud.com>
2021-08-29 06:56:13 -0500

    usmall style changes

--- react-dashboard/src/layouts/home.jsx
+++ react-dashboard/src/layouts/home.jsx
@@ -1,8 +1,18 @@
 import { Login } from "../services/login";
+import { useEffect } from "react";
 import "./home.css";
 import background from "../assets/img/sidebar-4.jpg";
 
-function Home() {
+function Home(props) {
+  useEffect(() => {
+    // set css properties
+    const backgroundImg = props.body;
+
+    // return function to clean up css properties
+    return () => {
+      backgroundImg;
+    };
+  }, []);
   return (
     <div style={{ backgroundImage: `url(${background})` }} className="home-img">
       <h1 className="home-title">Welcome to WeightN!</h1>
